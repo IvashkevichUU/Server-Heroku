93f9aaaf0d4c0c4ce60307a8eb9359d0a3a88255
IvashkevichUU <verstack@openledger.info>
2017-07-11 16:16:45 +0300

    create table students v4

--- main.go
+++ main.go
@@ -34,8 +34,6 @@ func createdb(c *gin.Context) {
 			fmt.Sprintf("Error creating database table: %q", err))
 		return
 	}
-
-
 	if _, err := db.Exec("INSERT INTO students (fio, info, score) VALUES ('Vasily Romanov', 'company: Mail.ru Group', '10')"); err != nil {
 		c.String(http.StatusInternalServerError,
 			fmt.Sprintf("Error incrementing tick: %q", err))
